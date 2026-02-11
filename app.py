7ec02d13617230c0d82d9e1561bb51e810490a37
goose-ws <goose@goose.ws>
2026-02-11 14:34:57 -0500

    Release v4.2.1

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 app_config = load_config()
 app.secret_key = app_config.get('flask_secret_key', 'fallback_dev_key_if_config_fails')
 
-APP_VERSION = '4.2.0'
+APP_VERSION = '4.2.1'
 @app.context_processor
 def inject_version():
     return dict(app_version=APP_VERSION)
