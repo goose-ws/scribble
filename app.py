c2c1c8ff8ed75a8a711857692728663d452c960f
goose-ws <goose@goose.ws>
2026-05-20 19:49:23 -0400

    v4.3.4

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ app = Flask(__name__)
 app_config = load_config()
 app.secret_key = app_config.get('flask_secret_key', 'fallback_dev_key_if_config_fails')
 
-APP_VERSION = '4.3.3'
+APP_VERSION = '4.3.4'
 
 def apply_transcript_options(text, campaign, session=None):
     """
